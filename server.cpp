d84c120916c4c741716846392a04e54018ecd2fd
bingma03 <bingma03@163.com>
2021-02-05 20:38:19 +0800

    change server.cpp file

--- server.cpp
+++ server.cpp
@@ -51,7 +51,7 @@ int main(int argc, char** argv) {
 		buf[n] = '\n';
 		printf("receive message from clinet: %s\n", buf);
 		
-		close(listenfd);
+		close(connfd);
 		return 0;
 	}
 	close(listenfd);
